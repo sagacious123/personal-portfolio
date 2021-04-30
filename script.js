0c88961f0305d370a7cb08e17d8b9a1e96797f50
bajegbo joseph <damilolaj23@gmail.com>
2021-04-30 17:57:31 +0100

    Total adjustment

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const navToggler = document.querySelector(".nav-toggler");
 const header = document.querySelector("header");
 const navItems = document.querySelector(".nav-items");
 
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display")
+    }, 4000);
+}
 
 navToggler.addEventListener("click", function() {
     header.classList.toggle("responsive");
@@ -19,17 +24,46 @@ window.addEventListener("scroll", function() {
     }
 })
 
-const navLink = document.querySelectorAll(".nav-items a");
-for(var i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener("click", function(e) {
 
-        if(e.target) {
-            navLink[0].classList.add("active")
-        }
-        // if(!this.classList.contains("active")) {
-        // this.classList.add("active");
-        // } else {
-        //     this.classList.remove("active");
-        // }
-    })
+
+var btnContainer = document.querySelector(".nav");
+
+// Get all buttons with class="btn" inside the container
+const navLink = document.querySelectorAll(".nav a");
+// const showActive = document.querySelector(".show-active")
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var d = 0; d < navLink.length; d++) {
+    // for (var j = 0; i < showActive.length; j++) {
+        window.addEventListener('scroll', function() {
+            const links = document.querySelectorAll('.nav-items a');
+            const sections = document.querySelectorAll('.show-active');
+
+            function changeLinkState() {
+            let i = sections.length;
+
+            while(--i && window.scrollY < sections[i].offsetTop && window.scrollY + 400 < sections[2].offsetTop) {}
+            
+            links.forEach((link) => link.classList.remove('active'));
+            links[i].classList.add('active');
+            }
+
+            changeLinkState();
+            // window.addEventListener('scroll', changeLinkState);
+        })
+
+        
+
+        navLink[d].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+        
+            // If there's no active class
+            if (current.length > 0) {
+              current[0].className = current[0].className.replace(" active", "");
+            }
+        
+            // Add the active class to the current/clicked button
+            this.className += " active";
+          });
+//     }
 }
