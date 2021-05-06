eb9f7b66937d6a74f8342f0f671b4ee69699939d
bajegbo joseph <damilolaj23@gmail.com>
2021-05-06 15:30:26 +0100

    Hero changed

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ window.onload = () => {
     }, 4000);
 }
 
-navToggler.addEventListener("click", function() {
-    header.classList.toggle("responsive");
-    this.classList.toggle("change");
-})
+navToggler.addEventListener("click", toggleNav())
 
 window.addEventListener("scroll", function() {
     if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50 ) {
@@ -25,45 +22,43 @@ window.addEventListener("scroll", function() {
 })
 
 
-
-var btnContainer = document.querySelector(".nav");
-
-// Get all buttons with class="btn" inside the container
 const navLink = document.querySelectorAll(".nav a");
-// const showActive = document.querySelector(".show-active")
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var d = 0; d < navLink.length; d++) {
-    // for (var j = 0; i < showActive.length; j++) {
-        window.addEventListener('scroll', function() {
-            const links = document.querySelectorAll('.nav-items a');
-            const sections = document.querySelectorAll('.show-active');
+    window.addEventListener('scroll', function() {
+        const links = document.querySelectorAll('.nav-items a');
+        const sections = document.querySelectorAll('.show-active');
 
-            function changeLinkState() {
-            let i = sections.length;
+        function changeLinkState() {
+        let i = sections.length;
 
-            while(--i && window.scrollY < sections[i].offsetTop && window.scrollY + 400 < sections[2].offsetTop) {}
-            
-            links.forEach((link) => link.classList.remove('active'));
-            links[i].classList.add('active');
-            }
-
-            changeLinkState();
-            // window.addEventListener('scroll', changeLinkState);
-        })
-
+        while(--i && window.scrollY + 50 < sections[i].offsetTop && window.scrollY + 400 < sections[2].offsetTop) {}
         
+        links.forEach((link) => link.classList.remove('active'));
+        links[i].classList.add('active');
+        }
 
-        navLink[d].addEventListener("click", function() {
-            var current = document.getElementsByClassName("active");
-        
-            // If there's no active class
-            if (current.length > 0) {
-              current[0].className = current[0].className.replace(" active", "");
-            }
-        
-            // Add the active class to the current/clicked button
-            this.className += " active";
-          });
-//     }
+        changeLinkState();
+    })
+
+    navLink[d].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+    
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+    
+        this.className += " active";
+        });
 }
+
+
+function toggleNav() {
+    return function () {
+        header.classList.toggle("responsive");
+        this.classList.toggle("change");
+    };
+}
+//FUNCTIONS
